f10928552f2a8c62502b7e46396a0af547ddc1db
Pasha <pokikavsky22@codingburgas.bg>
2026-04-10 10:13:37 +0300

    save

--- SleepTracker/Program.cs
+++ SleepTracker/Program.cs
@@ -1,8 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using SleepTracker.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite("Data Source=sleeptracker.db"));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
